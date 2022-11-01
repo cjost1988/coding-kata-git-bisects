f1873ce39537b978348155d23d5a6b24ee155af6
Christian Jost <christian.jost@schuettflix.com>
2022-11-01 23:35:16 +0100

    feat: some commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ enum ColorEnum {
 }
 
 const matrix: Array<Array<ColorEnum>> = [
-    [ColorEnum.RED, ColorEnum.RED, ColorEnum.RED],
+    [ColorEnum.YELLOW, ColorEnum.RED, ColorEnum.RED],
     [ColorEnum.RED, ColorEnum.YELLOW, ColorEnum.RED],
     [ColorEnum.RED, ColorEnum.RED, ColorEnum.RED],
 ]
