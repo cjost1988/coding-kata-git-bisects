d2e6a7442c54d169ea8990d5a095f35b54bed67b
Christian Jost <christian.jost@schuettflix.com>
2022-11-01 23:30:18 +0100

    feat: some commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+enum ColorEnum {
+    GREEN='green',
+    YELLOW='yellow',
+    RED='red',
+}
 
-    </div>
-  );
+const matrix: Array<Array<ColorEnum>> = [
+    [ColorEnum.YELLOW, ColorEnum.GREEN, ColorEnum.YELLOW],
+    [ColorEnum.GREEN, ColorEnum.RED, ColorEnum.YELLOW],
+    [ColorEnum.GREEN, ColorEnum.YELLOW, ColorEnum.YELLOW],
+]
+
+function App() {
+    return (
+        <div className="App">
+            <div className="color-matrix">
+              {matrix.flat(1).map((color: ColorEnum) => <div className={color.toString()}>&nbsp;</div>)}
+            </div>
+        </div>
+    );
 }
 
 export default App;
